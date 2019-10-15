a105b3dcc7b71ab2c070cf88054fc3ed301231f1
枳本望 <nozomusp29@gmail.com>
2019-10-15 22:23:07 +0800

    今日の課題
    修正版

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,20 +5,6 @@
 //   });
 // });
 $(function() {
-  // alert()
-  //ボタンのサイズ変更
-  $("#js-click-btn").on("click", function() {
-    $(this).addClass("large-btn");
-  });
-
- //ボタンの色変更
- $("#js-hover-btn")
- .on("mouseover", function() {
-   $(this).addClass("opacity");
- })
- .on("mouseleave", function() {
-   $(this).removeClass("opacity");
- });
  //要素の表示切り替え
  $("#js-show-btn").on("click", function() {
   $("#js-target-element").fadeIn(1000);
@@ -26,26 +12,5 @@ $(function() {
 
 $("#js-hide-btn").on("click", function() {
   $("#js-target-element").fadeOut(2000);
-});
-
-// 要素の追加
-$('#js-add-btn').on('click',function() {
-  let beforeBtn = $('<li>')
-  .addClass('btn')
-  .text('ボタンの前に追加');
-
-  let afterBtn=$('<li>')
-  .addClass('btn')
-  .text('ボタンの後ろに追加');
-
-  $(this).before(beforeBtn);
-  $(this).after(afterBtn);
-
-  // 要素の前後（中）に追加
-  $('#js-add-btn2').on('click', function() {
-    $(this).prepend('前');
-  $(this).append('後');
-  });
-
 });
 });
